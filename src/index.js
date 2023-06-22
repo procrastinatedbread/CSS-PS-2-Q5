e05a0a9ea76ece8a4113d959e30dd4cc35b16872
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 05:00:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div class="card">
+<h2>Basic Plan</h2>
+<p>$29.99/mo</p>
+
+<ul>
+<li>For family who live together</li>
+<li>Ad free music listening</li>
+<li>Download 10k songs/device</li>
+<li>Login via 5 devices</li>
+</ul>
+
+<button>Sign Up</button>
+
+  
+</div>
+`;
